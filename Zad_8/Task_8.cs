e8e6e95564cbd936bc139454e38ee749b58ccf61
Vyachaslav75 <kslava@tut.by>
2022-10-01 13:37:55 +0300

    add Task 8

---
+++ Zad_8/Task_8.cs
@@ -0,0 +1,13 @@
+﻿//Домашнее задание задача 8
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+int i = 2;
+
+Console.Write(userNumber + " -> ");
+Console.Write(i);
+while (i + 2 <= userNumber)
+{
+    i += 2;
+    Console.Write(", "+i);
+}
