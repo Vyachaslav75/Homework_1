7efaebf5ca1c07448520c528b4e5313cf6608c52
Vyachaslav75 <kslava@tut.by>
2022-10-01 13:37:26 +0300

    add Task 6

---
+++ Zad_6/Task_6.cs
@@ -0,0 +1,14 @@
+﻿//Домашнее задание задача 6
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+double userNumber = int.Parse(userInput);
+
+Console.Write(userNumber + " -> ");
+if (userNumber%2==0)
+{
+    Console.WriteLine(" да ");
+}
+else
+{
+    Console.WriteLine(" нет ");
+}
